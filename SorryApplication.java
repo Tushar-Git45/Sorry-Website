c3e1f1c789714f85cb8684526799c88b7a3085aa
Tushar-Git45 <tushsrivastava45@gmail.com>
2026-01-17 22:23:18 +0530

    Add files via upload

---
+++ SorryApplication.java
@@ -0,0 +1,12 @@
+package com.example.sorry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorryApplication.class, args);
+    }
+}
